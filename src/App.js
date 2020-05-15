ae814dcfd261374d61f45cecb20b86f97e7e8399
JoseRojas96 <jose.rojas@ing.austral.edu.ar>
2020-05-15 15:32:38 -0300

    Added template

--- src/App.js
+++ src/App.js
@@ -1,16 +1,123 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
-class App extends Component {
+function App() {
 
-  render() {
+    const openMenu = () => {
+        document.querySelector(".sidebar").classList.add("open");
+    };
+
+    const closeMenu = () => {
+        document.querySelector(".sidebar").classList.remove("open");
+    };
+
     return (
-        <div className="App">
-          <h1 className="alert alert-warning">Welcome to the e-commerce</h1>
+        <div className="grid-container">
+            <header className="header">
+                <div className="brand">
+                    <button onClick={openMenu}>
+                        &#9776;
+                    </button>
+                    <a href="index.html">amazona</a>
+                </div>
+                <div className="header-links">
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Sign In</a>
+                </div>
+            </header>
+            <aside className="sidebar">
+                <h3>Shopping Categories</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                <ul>
+                    <li>
+                        <a href="index.html">Pants</a>
+                    </li>
+
+                    <li>
+                        <a href="index.html">Shirts</a>
+                    </li>
+
+                </ul>
+            </aside>
+            <main className="main">
+                <div className="content">
+                    <ul className="products">
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="images/d1.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="images/d1.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="images/d1.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="images/d1.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="images/d1.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="images/d1.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+
+            </main>
+            <footer className="footer">
+                All right reserved.
+            </footer>
         </div>
     );
-  }
 }
 
 export default App;
