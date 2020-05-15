67dcee332173a2dc95fd184c647e366eae3be6ea
JoseRojas96 <jose.rojas@ing.austral.edu.ar>
2020-05-15 16:20:33 -0300

    Added data.js and react-router-dom dependecy

--- src/App.js
+++ src/App.js
@@ -1,123 +1,73 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import data from "./data";
 
-function App() {
+class App extends Component {
 
-    const openMenu = () => {
+    openMenu() {
         document.querySelector(".sidebar").classList.add("open");
-    };
+    }
 
-    const closeMenu = () => {
+    closeMenu() {
         document.querySelector(".sidebar").classList.remove("open");
-    };
+    }
 
-    return (
-        <div className="grid-container">
-            <header className="header">
-                <div className="brand">
-                    <button onClick={openMenu}>
-                        &#9776;
-                    </button>
-                    <a href="index.html">amazona</a>
-                </div>
-                <div className="header-links">
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">Sign In</a>
-                </div>
-            </header>
-            <aside className="sidebar">
-                <h3>Shopping Categories</h3>
-                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-                <ul>
-                    <li>
-                        <a href="index.html">Pants</a>
-                    </li>
+    render() {
+        return (
+            <div className="grid-container">
+                <header className="header">
+                    <div className="brand">
+                        <button onClick={this.openMenu}>
+                            &#9776;
+                        </button>
+                        <a href="index.html">amazona</a>
+                    </div>
+                    <div className="header-links">
+                        <a href="cart.html">Cart</a>
+                        <a href="signin.html">Sign In</a>
+                    </div>
+                </header>
+                <aside className="sidebar">
+                    <h3>Shopping Categories</h3>
+                    <button className="sidebar-close-button" onClick={this.closeMenu}>x</button>
+                    <ul>
+                        <li>
+                            <a href="index.html">Pants</a>
+                        </li>
 
-                    <li>
-                        <a href="index.html">Shirts</a>
-                    </li>
-
-                </ul>
-            </aside>
-            <main className="main">
-                <div className="content">
-                    <ul className="products">
                         <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
+                            <a href="index.html">Shirts</a>
                         </li>
 
                     </ul>
-                </div>
+                </aside>
+                <main className="main">
+                    <div className="content">
+                        <ul className="products">
+                            {
+                                data !== undefined && data.products.map((product, key) => (
+                                    <li key={key}>
+                                        <div className="product">
+                                            <img className="product-image" src={product.image} alt="product"/>
+                                            <div className="product-name">
+                                                <a href="product.html">{product.name}</a>
+                                            </div>
+                                            <div className="product-brand">{product.brand}</div>
+                                            <div className="product-price">${product.price}</div>
+                                            <div className="product-rating">{product.rating} Stars ({product.reviews} Reviews)</div>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
 
-            </main>
-            <footer className="footer">
-                All right reserved.
-            </footer>
-        </div>
-    );
+                </main>
+                <footer className="footer">
+                    All right reserved.
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,85 @@
+export default {
+    products: [
+        {
+            name: "Slim Shirt",
+            category: "Shirts",
+            brand: "Adidas",
+            price: 60,
+            rating: 4.2,
+            reviews: 10,
+            image: "images/d1.jpg"
+        },
+        {
+            name: "Fit Shirt",
+            category: "Shirts",
+            brand: "Adidas",
+            price: 65,
+            rating: 4.4,
+            reviews: 8,
+            image: "images/d1.jpg"
+        },
+        {
+            name: "Best Shirt",
+            category: "Shirts",
+            brand: "Adidas",
+            price: 63,
+            rating: 4.7,
+            reviews: 12,
+            image: "images/d1.jpg"
+        },
+        {
+            name: "Slim Shirt",
+            category: "Shirts",
+            brand: "Nike",
+            price: 70,
+            rating: 4.2,
+            reviews: 10,
+            image: "images/d1.jpg"
+        },
+        {
+            name: "Fit Shirt",
+            category: "Shirts",
+            brand: "Nike",
+            price: 75,
+            rating: 4.4,
+            reviews: 8,
+            image: "images/d1.jpg"
+        },
+        {
+            name: "Best Shirt",
+            category: "Shirts",
+            brand: "Nike",
+            price: 73,
+            rating: 4.7,
+            reviews: 12,
+            image: "images/d1.jpg"
+        },
+        {
+            name: "Slim Shirt",
+            category: "Shirts",
+            brand: "Wilson",
+            price: 50,
+            rating: 4.2,
+            reviews: 10,
+            image: "images/d1.jpg"
+        },
+        {
+            name: "Fit Shirt",
+            category: "Shirts",
+            brand: "Wilson",
+            price: 55,
+            rating: 4.4,
+            reviews: 8,
+            image: "images/d1.jpg"
+        },
+        {
+            name: "Best Shirt",
+            category: "Shirts",
+            brand: "Wilson",
+            price: 53,
+            rating: 4.7,
+            reviews: 12,
+            image: "images/d1.jpg"
+        }
+    ]
+};
